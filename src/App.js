69e6eed23161149cded2f19999a5dbda405be705
YashNaj <yashalnajeeb@gmail.com>
2022-06-29 19:14:07 -0700

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js
     Untracked files:
    src/assets/
    	src/babel-plugin-macros.config.js
    	src/babel.config.js
    	src/components/
    	src/containers/
    
    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Header, Footer, Welcome, PricePage, About } from "./containers";
+import { Cta, Card } from "./components";
+import Contact from "./containers/contact/Contact";
+import Studentportal from "./containers/studentportal/Studentportal";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient_bg">
+        <Header />
+        <Welcome />
+        <About />
+        <PricePage />
+        <Contact />
+        <Studentportal/>
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
